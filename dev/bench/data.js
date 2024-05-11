e950a5ae35d3d3e19d109cd2ee575f2f9b4891a7
github-action-benchmark <github@users.noreply.github.com>
2024-05-11 18:11:40 +0000

    add Benchmark (benchmarkjs) benchmark result for 6d4cf8281c9de987767855393db617a7f5541e5a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715450648039,
+  "lastUpdate": 1715451100111,
   "repoUrl": "https://github.com/solo-daemon/cacti",
   "entries": {
     "Benchmark": [
@@ -38,6 +38,37 @@ window.BENCHMARK_DATA = {
             "range": "±1.21%",
             "unit": "ops/sec",
             "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "petermetz@users.noreply.github.com",
+            "name": "Peter Somogyvari",
+            "username": "petermetz"
+          },
+          "distinct": true,
+          "id": "6d4cf8281c9de987767855393db617a7f5541e5a",
+          "message": "build(deps): bump rustls in /weaver/common/protos-rs\n\nBumps [rustls](https://github.com/rustls/rustls) from 0.21.10 to 0.21.11.\n- [Release notes](https://github.com/rustls/rustls/releases)\n- [Changelog](https://github.com/rustls/rustls/blob/main/CHANGELOG.md)\n- [Commits](https://github.com/rustls/rustls/compare/v/0.21.10...v/0.21.11)\n\n---\nupdated-dependencies:\n- dependency-name: rustls\n  dependency-type: indirect\n...\n\nSigned-off-by: dependabot[bot] <support@github.com>",
+          "timestamp": "2024-05-11T07:15:41-07:00",
+          "tree_id": "9567ea1751cc7626de0e71737011210f62e1e035",
+          "url": "https://github.com/solo-daemon/cacti/commit/6d4cf8281c9de987767855393db617a7f5541e5a"
+        },
+        "date": 1715451098943,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 729,
+            "range": "±2.86%",
+            "unit": "ops/sec",
+            "extra": "179 samples"
           }
         ]
       }
